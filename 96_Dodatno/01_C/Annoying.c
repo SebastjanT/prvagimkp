c86d972d3c14f3185b8d45ac2ccf32737215f4da
SebastjanT <sebastjan.tkavc@gmail.com>
2022-05-17 14:49:30 +0000

    Fixed indentations.

--- 96_Dodatno/01_C/Annoying.c
+++ 96_Dodatno/01_C/Annoying.c
@@ -4,10 +4,10 @@
 
 int main(int argc, char *argv[]){
   // Infinate loop that repeats the users input back to them, if there is any (with many errors waiting to happen)
-	char userInput[MAX_INPUT];
+  char userInput[MAX_INPUT];
 
-	while ((fgets(userInput, MAX_INPUT, stdin) != NULL) && (userInput[0] != '\n')){
-		printf("%s", userInput);
+  while ((fgets(userInput, MAX_INPUT, stdin) != NULL) && (userInput[0] != '\n')){
+    printf("%s", userInput);
   }
 
   return 0;
